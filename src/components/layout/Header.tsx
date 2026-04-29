e8a0ff75c04a53cc9ff1dd3673c460112be7420f
Jean Yves Sadio <jeanyvessadio7@gmail.com>
2026-04-29 12:20:08 +0000

    Page inscription

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -22,7 +22,6 @@ export default function Header() {
   const navbarItems = [
     { name: "Accueil", href: "/" },
     { name: "À propos", href: "/#about" },
-    { name: "Inscription", href: "/sign-up" },
     { name: "Contact", href: "/#contact" },
   ];
 
